460be1ffa55f95c8a5cacd6a5c8ca3284e6123e5
Simon Schagerer <simon.schagerer@gmail.com>
2020-01-22 21:39:44 +0100

    Add about page

---
+++ ImageFinderUserStudy/ImageFinderUserStudyWeb/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImageFinderUserStudyWeb.Pages
+{
+    public class About : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
