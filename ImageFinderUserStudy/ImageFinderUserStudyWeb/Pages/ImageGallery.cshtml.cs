033f93fb006b9a295aecf0447072796d345ccf9a
schagsim <56699561+schagsim@users.noreply.github.com>
2020-01-22 01:20:55 +0100

    Minimal version user story (#3)
    * Add .idea ignore to .gitignore
    
    * Initial project commit
    
    * Change home-page titles
    
    * Add basic flow to image presentation
    
    * Add ignore of google images in web resources to .gitignore
    
    * Add another flow step
    
    * Add mock images to image gallery
    
    * Finish thank you page

---
+++ ImageFinderUserStudy/ImageFinderUserStudyWeb/Pages/ImageGallery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImageFinderUserStudyWeb.Pages
+{
+    public class ImageGallery : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ ImageFinderUserStudy/ImageFinderUserStudyWeb/Pages/SummaryPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ImageFinderUserStudyWeb.Pages
+{
+    public class SummaryPage : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
