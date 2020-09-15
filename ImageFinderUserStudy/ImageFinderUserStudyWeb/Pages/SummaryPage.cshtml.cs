9098769ddf46d7a0f6f6779c43382f0476ef5793
Simon Schagerer <56699561+schagsim@users.noreply.github.com>
2020-09-15 14:56:42 +0200

    Add user session (#15)
    * Add user session info class
    
    * Add user sessions manager
    
    * Include Json convertor package
    
    * Prepare summary page for Json conversion
    
    * Add TODO for Json conversion (will be moved to DB)
    
    * Add userSessionsManager to DI container
    
    Co-authored-by: simon <simon.schagerer@blindspot.ai>

--- ImageFinderUserStudy/ImageFinderUserStudyWeb/Pages/SummaryPage.cshtml.cs
+++ ImageFinderUserStudy/ImageFinderUserStudyWeb/Pages/SummaryPage.cshtml.cs
@@ -1,12 +1,45 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace ImageFinderUserStudyWeb.Pages
 {
     public class SummaryPage : PageModel
     {
-        public void OnGet()
+        private readonly UserSessionsManager _userSessionsManager;
+        
+        private UserSessionInfo UserSession { get; set; }
+
+        public SummaryPage(
+            UserSessionsManager userSessionsManager
+        )
         {
-            
+            _userSessionsManager = userSessionsManager;
+        }
+        
+        public void OnGetSummaryPageAsync(
+            Guid userSessionId
+        )
+        {
+            try
+            {
+                UserSession = _userSessionsManager.UserSessions[userSessionId];
+                var filePath = "userSessions/" + UserSession.UserSessionId + ".json";
+                // TODO: We will later move this to DB instead of file output so it can be queried.
+                System.IO.File.WriteAllText(filePath, JsonConvert.SerializeObject(UserSession));
+            }
+            catch (KeyNotFoundException e)
+            {
+                // TODO: Add a logger, not a console logging.
+                Console.WriteLine(e);
+            }
+            // TODO: Add a better exception catching? The "WriteAllText" can throw like 10 exceptions.
+            catch (Exception eDefault)
+            {
+                // TODO: Add a logger, not a console logging.
+                Console.WriteLine(eDefault);
+            }
         }
     }
 }
