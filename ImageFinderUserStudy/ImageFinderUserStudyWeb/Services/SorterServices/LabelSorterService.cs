0f1047ec73e981812e0cd1d001837d20527c2214
Simon Schagerer <simon.schagerer@gmail.com>
2020-09-17 01:00:32 +0200

    Add label parsing to LabelSorterService

--- ImageFinderUserStudy/ImageFinderUserStudyWeb/Services/SorterServices/LabelSorterService.cs
+++ ImageFinderUserStudy/ImageFinderUserStudyWeb/Services/SorterServices/LabelSorterService.cs
@@ -1,7 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ImageFinderUserStudyWeb.DataContracts;
+
 namespace ImageFinderUserStudyWeb.Services.SorterServices
 {
     public class LabelSorterService
     {
+        public List<ImageLabels> ParseLabels(string pathToLabelsFolder)
+        {
+            if (pathToLabelsFolder == null)
+            {
+                throw new ArgumentNullException(nameof(pathToLabelsFolder));
+            }
+
+            var parsedImageLabels = new List<ImageLabels>();
+
+            var labelFilesNames = Directory.GetFiles(pathToLabelsFolder);
+            foreach (var labelFileName in labelFilesNames)
+            {
+                string[] currentLabelsFile;
+                try
+                {
+                    var labelsFile = File.ReadAllText($"{labelFileName}");
+                    currentLabelsFile = Regex.Split(labelsFile, "(, \")|(,\")")
+                        .Select(x => x.Trim(new char[] { '"', '{', '}', ',' }))
+                        .Where(x => !string.IsNullOrWhiteSpace(x))
+                        .ToArray();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine($"Couldn't read file {labelFileName}.");
+                    continue;
+                }
+
+                var currentLabels = new Dictionary<string, double>();
+                foreach (var t in currentLabelsFile)
+                {
+                    var splitLabel = Regex.Split(t, "(:)")
+                        .Select(x => x.Trim(new char[] { '"', '{', '}', ',', ':' }))
+                        .Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+                    if (splitLabel.Length != 2) goto NEXT;
+
+                    double.TryParse(splitLabel[1], out double labelVal);
+                    currentLabels.Add(splitLabel[0], labelVal);
+                }
+                parsedImageLabels.Add(
+                    new ImageLabels(
+                        Path.GetFileNameWithoutExtension(labelFileName),
+                        currentLabels
+                    )
+                );
+                NEXT:;
+            }
+
+            return parsedImageLabels;
+        }
         
+        public SortersDtos.SorterOutput SortLabels(int numberOfImagesToPresent)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
