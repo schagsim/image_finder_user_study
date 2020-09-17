115ed5190f3ae9a8979afcd17f02d6ca9b181b9f
Simon Schagerer <simon.schagerer@gmail.com>
2020-09-17 22:56:50 +0200

    Add a new testing project (the old one will be removed later)

---
+++ ImageFinderUserStudy/ImageFinderUserStudyTest/SortersTest/LabelSorterServiceTest.cs
@@ -0,0 +1,14 @@
+using ImageFinderUserStudyWeb.Services.SorterServices;
+using Xunit;
+
+namespace ImageFinderUserStudyTest.SortersTest
+{
+    public class LabelSorterServiceTest
+    {
+        [Fact]
+        public void SortOutputCorrect()
+        {
+            var sorterService = new LabelSorterService();
+        }
+    }
+}
