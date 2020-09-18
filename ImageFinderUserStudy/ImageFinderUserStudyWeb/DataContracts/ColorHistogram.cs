dd46b20f30c07d5413ad2d619067f89f0a345d2e
Simon Schagerer <simon.schagerer@gmail.com>
2020-09-18 23:51:19 +0200

    Add histogram comparison structure for hist comparison dictionary

--- ImageFinderUserStudy/ImageFinderUserStudyWeb/DataContracts/ColorHistogram.cs
+++ ImageFinderUserStudy/ImageFinderUserStudyWeb/DataContracts/ColorHistogram.cs
@@ -32,4 +32,27 @@ namespace ImageFinderUserStudyWeb.DataContracts
             RedHistogram = redHistogram;
         }
     }
+
+    public class HistogramComparisonKeys
+    {
+        public string Key1 { get; }
+        public string Key2 { get; }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is HistogramComparisonKeys secondHistogram)) return false;
+            return Key1 == secondHistogram.Key1 && Key2 == secondHistogram.Key2;
+        }
+
+        public override int GetHashCode() => new {Key1, Key2}.GetHashCode();
+
+        public HistogramComparisonKeys(
+            string key1,
+            string key2
+        )
+        {
+            Key1 = key1;
+            Key2 = key2;
+        }
+    }
 }
