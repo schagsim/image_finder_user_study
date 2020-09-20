a7ec8725b7e3760927b0915f250f4546d9f424e4
Simon Schagerer <simon.schagerer@gmail.com>
2020-09-20 22:05:40 +0200

    Allow adding new user session to session manager

--- ImageFinderUserStudy/ImageFinderUserStudyWeb/UserSessionsManager.cs
+++ ImageFinderUserStudy/ImageFinderUserStudyWeb/UserSessionsManager.cs
@@ -5,11 +5,21 @@ namespace ImageFinderUserStudyWeb
 {
     public class UserSessionsManager
     {
-        public Dictionary<Guid, UserSessionInfo> UserSessions { get; private set; }
+        public Dictionary<Guid, UserSessionInfo> UserSessions { get; }
 
         public UserSessionsManager()
         {
             UserSessions = new Dictionary<Guid, UserSessionInfo>();
+        }
+
+        public void AddNewSessionInfo(
+            Guid userSessionId
+        )
+        {
+            var newUserSession =
+                new UserSessionInfo(
+                    userSessionId
+                );
         }
     }
 }
