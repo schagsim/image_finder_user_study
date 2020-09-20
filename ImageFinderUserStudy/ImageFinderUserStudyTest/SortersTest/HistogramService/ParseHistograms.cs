fb5ac1ffe6bce19f8db2357f857593ce254a5dd7
Simon Schagerer <simon.schagerer@gmail.com>
2020-09-20 16:58:44 +0200

    Add histogram parser test to new testing project

---
+++ ImageFinderUserStudy/ImageFinderUserStudyTest/SortersTest/HistogramService/ParseHistograms.cs
@@ -0,0 +1,50 @@
+using System.IO;
+using System.Linq;
+using ImageFinderUserStudyWeb.Services.SorterServices;
+using Xunit;
+
+namespace ImageFinderUserStudyTest.SortersTest.HistogramService
+{
+    public class ParseHistograms
+    {
+        private const string TestFilesFolder = @"TestResources/TestColorHistograms";
+        
+        [Fact]
+        public void CorrectParse()
+        {
+            var histogramService = new HistogramSorterService();
+            
+            // Get the directory in which the tests are contained.
+            var pathToHistogramFiles = $"{Directory.GetCurrentDirectory()}/../../../{TestFilesFolder}";
+            
+            // Parse the files in the test directory.
+            var parseOutput = histogramService.ParseHistograms(pathToHistogramFiles);
+            
+            var testFileNames =
+                Directory
+                    .GetFiles(pathToHistogramFiles)
+                    .Select(Path.GetFileNameWithoutExtension)
+                    .ToList();
+            
+            var imageIds =
+                parseOutput
+                    .Select(histogram => histogram.ImageId)
+                    .ToList();
+
+            Assert.True(testFileNames.Count == parseOutput.Count);
+            foreach (var testFileName in testFileNames)
+            {
+                Assert.Contains(testFileName, imageIds);
+            }
+
+            foreach (var colorHistogram in parseOutput)
+            {
+                Assert.True(colorHistogram.RedHistogram.Count == 16);
+                Assert.True(colorHistogram.GreenHistogram.Count == 16);
+                Assert.True(colorHistogram.BlueHistogram.Count == 16);
+            }
+            
+            
+        }
+    }
+}
